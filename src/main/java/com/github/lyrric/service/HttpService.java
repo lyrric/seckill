551106bb17f519e5d5b0d5b34c00da155b0e89c4
wangxiaodong <xx20510@163.com>
2020-08-10 11:29:02 +0800

    修改了获取接种日期的传参参数错误

--- src/main/java/com/github/lyrric/service/HttpService.java
+++ src/main/java/com/github/lyrric/service/HttpService.java
@@ -60,8 +60,8 @@ public class HttpService {
     public List<SubDate> getSkSubDays(String vaccineId, String orderId) throws IOException, BusinessException {
         String path = baseUrl+"/seckill/seckill/subscribeDays.do";
         Map<String, String> params = new HashMap<>();
-        params.put("seckillId", vaccineId);
-        params.put("vaccineIndex", orderId);
+        params.put("id", vaccineId);
+        params.put("sid", orderId);
         String json =  get(path, params);
         logger.info("日期格式:{}", json);
         return JSONObject.parseArray(json).toJavaList(SubDate.class);
